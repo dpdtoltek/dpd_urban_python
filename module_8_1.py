902489d81ecbd1464a238423d115fbca238989b6
Pavel <dpd2003@gmail.com>
2024-08-20 16:00:17 +0300

    Задание "Программистам всё можно"

---
+++ module_8_1.py
@@ -0,0 +1,14 @@
+def add_everything_up(a, b):
+    try:
+        sum_1 = a + b
+        return round(sum_1, 3)
+    except TypeError:
+        a = str(a)
+        b = str(b)
+        sum_2 = a + b
+        return sum_2
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
