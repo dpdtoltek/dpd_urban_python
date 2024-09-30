ae2add903b412458fb3ff0d30168083fca16daf7
Pavel <dpd2003@gmail.com>
2024-09-30 19:23:33 +0300

    Update module_11_intro.py
    Разделил атрибуты и методы и добавил 'value'

--- module_11_intro.py
+++ module_11_intro.py
@@ -1,7 +1,6 @@
 import inspect
 import os
 
-
 cur_dir = os.getcwd()
 
 
@@ -11,18 +10,24 @@ class Intro:
 
 
 def introspection_info(obj):
-    obj_info = {'type': type(obj), 'attributes': [getattr(obj, attr) for attr in dir(obj)], 'methods': dir(obj),
-                'module': inspect.getmodule(introspection_info), 'func_name': introspection_info.__name__}
+    obj_info = {'type': type(obj)}
     if isinstance(obj, Intro):
+        obj_info['value'] = repr(obj.attribute)
         obj_info['class_name'] = Intro.__name__
+    elif not isinstance(obj, Intro):
+        obj_info['value'] = repr(obj)
+    obj_info['func_name'] = introspection_info.__name__
     obj_info['id'] = id(obj)
+    obj_info['attributes'] = [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
+    obj_info['methods'] = [meth for meth in dir(obj) if callable(getattr(obj, meth))]
+    obj_info['module'] = inspect.getmodule(introspection_info)
     return obj_info
 
 
 arg_info = introspection_info(42)
 print(arg_info)
 
-arg_1 = Intro(42)
+arg_1 = Intro(attribute=42)
 arg_info = introspection_info(arg_1)
 print(arg_info)
 
