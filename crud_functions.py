bfc24a650a736463d5a2b8b5991257acfc6dff7d
Pavel <dpd2003@gmail.com>
2024-11-01 19:25:39 +0300

    Update crud_functions.py
    Модифицирован код под задачу "Регистрация покупателей"

--- crud_functions.py
+++ crud_functions.py
@@ -21,6 +21,18 @@ def initiate_db():
         if check_title.fetchone() is None:
             cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
                            (f'Продукт {i}', f'Описание {i}', f'{i * 100}'))
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    );
+    ''')
+
+    cursor.execute('CREATE INDEX IF NOT EXISTS idx_username ON Users (username)')
     connection.commit()
 
 
@@ -28,5 +40,20 @@ def get_all_products():
     cursor.execute('SELECT * FROM Products')
     products = cursor.fetchall()
     connection.commit()
-    connection.close()
     return products
+
+
+def add_user(username, email, age, balance=1000):
+    check_user = cursor.execute('SELECT * FROM Users WHERE email=?', (email,))
+    if check_user.fetchone() is None:
+        cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                       (username, email, age, balance))
+    connection.commit()
+
+
+def is_included(username):
+    check_user = cursor.execute('SELECT * FROM Users WHERE username=?', (username,))
+    if check_user.fetchone() is not None:
+        return True
+    else:
+        return False
