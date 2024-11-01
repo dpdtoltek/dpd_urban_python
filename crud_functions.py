ed96d5af7370581335ce3e65c1a1d1e022aa6a34
Pavel <dpd2003@gmail.com>
2024-11-01 11:35:51 +0300

    Update crud_functions.py
    Исключил повторное добавление продукта при инициализации базы данных.

--- crud_functions.py
+++ crud_functions.py
@@ -17,10 +17,11 @@ def initiate_db():
     cursor.execute('CREATE INDEX IF NOT EXISTS idx_title ON Products (title)')
 
     for i in range(1, 5):
-        cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
-                       (f'Продукт {i}', f'Описание {i}', f'{i * 100}'))
+        check_title = cursor.execute('SELECT * FROM Products WHERE title=?', (f'Продукт {i}',))
+        if check_title.fetchone() is None:
+            cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
+                           (f'Продукт {i}', f'Описание {i}', f'{i * 100}'))
     connection.commit()
-    connection.close()
 
 
 def get_all_products():
@@ -29,7 +30,3 @@ def get_all_products():
     connection.commit()
     connection.close()
     return products
-
-
-if __name__ == '__main__':
-    initiate_db()
